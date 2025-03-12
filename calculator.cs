b2447426a227064ac9275a02999468a03c2d6793
alfrinsetyaputra <alfrinsetyaputra@students.amikom.ac.id>
2025-03-12 14:14:34 +0700

    inisialisasi project

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,23 @@ class calculator
 {
 	static void main (string[] args)
 	{
+		int a = 10; 
+		int b = 6;
 		
+		console.writeline("Hasil Penambahan: {0} + {1} = {2}, a, b, Penambahan(a,b));
+		console.writeline("Hasil Pengurangan: {0} - {1} = {2}, a, b, Pengurangan(a,b));
+		
+		console.writeline("\nTekan sembarang key untuk keluar);
+		console.ReadKey();
+	
 	}
-
+	
+	static int penambahan(int a,int b)
+	{
+		return a + b;
+	}
+	static int pengurangan(int a, int b)
+	{
+		return a - b;
+	}
 }
